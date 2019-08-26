b22b8a0ea1f6381e3999362321fdc89ea2943944
keisuke sato <keisuke.sato.gp@gmail.com>
2019-08-26 19:41:45 +0900

    add Result.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <div class="quiz">
-      <p class="number">第{{ current + 1 }}問</p>
-      <p class="question">{{ questions[current].question }}</p>
-      <ul>
-        <li v-on:click="selectAnswer(index)" v-for="(question, index) in questions[current].answers" v-bind:key="question.id"><span>{{ index + 1 }}:</span>{{ question }}</li>
-      </ul>
+    <div class="box">
+      <div class="quiz" v-bind:class="{ finish: isFinish }">
+        <p class="number">第{{ current + 1 }}問</p>
+        <p class="question">{{ questions[current].question }}</p>
+        <ul class="answers">
+          <li v-on:click="selectAnswer(index)" v-for="(question, index) in questions[current].answers" v-bind:key="question.id">
+            <span class="num">{{ index + 1 }}</span>
+            <span class="text">{{ question }}</span>
+          </li>
+        </ul>
+      </div>
+      <transition name="fade">       
+        <Result v-bind:correct="correctAnswer" v-bind:length="questionLength" v-if="isFinish"></Result>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-// import App from './components/AnswerButton.vue'
+import Result from './view/Result.vue'
 
 export default {
   name: 'app',
@@ -20,6 +28,7 @@ export default {
       correctAnswer: 0,
       current: 0,
       questionLength: 3,
+      isFinish: false,
       questions: [
         {
           question: "ノンカロリーは何カロリー？",
@@ -51,28 +60,130 @@ export default {
       ]
     }
   },
+  components: {
+    Result
+  },
   methods: {
     selectAnswer: function(index){
+      // 正解数
       if(index == this.questions[this.current].answer - 1){
         this.correctAnswer ++
       }
-      
+      //
       if(this.current == this.questionLength - 1){
-        alert(this.correctAnswer);
+        this.isFinish = true
       } else {
         this.current ++
       }
-
-
     }
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background: #2f89fc;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+.box {
+  background: #fff;
+  width: 100%;
+  max-width: 600px;
+  min-height: 400px;
+  padding: 40px;
+  margin: 50px auto;
+  border-radius: 10px;
+  position: relative;
+}
+.quiz {
+  text-align: center;
+  &.finish {
+    display: none;
+  }
+  .number {
+    font-size: 20px;
+    margin: 0 0 20px;
+  }
+  .question {
+    font-size: 24px;
+    margin: 0 0 40px;
+
+  }
+}
+.answers {
+  margin: 0;
+  padding: 0;
+  li {
+    list-style: none;
+    background: #eee;
+    text-align: left;
+    margin: 0 0 10px;
+    cursor: pointer;
+    display: flex;
+    border-radius: 10px;
+    overflow: hidden;
+    span {
+      font-size: 18px;
+      line-height: 50px;
+    }
+    .num {
+      color: #fff;
+      background: #000;
+      width: 50px;
+      text-align: center;
+    }
+    .text {
+      padding: 0 20px;
+    }
+  }
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1.5s;
+}
+.fade-enter, .fade-leave-to  {
+  opacity: 0;
+}
+
+
+@media (max-width: 750px) {
+  #app {
+    padding: 10px;
+  }
+  .box {
+    margin: 0;
+    padding: 25px;
+  }
+  .quiz {
+    .number {
+      font-size: 16px;
+      margin: 0 0 10px;
+    }
+    .question {
+      font-size: 18px;
+      margin: 0 0 30px;
+
+    }
+  }
+  .answers {
+    li {
+      span {
+        font-size: 15px;
+      }
+      .text {
+        padding: 0 15px;
+      }
+    }
+  }
+}
+
 </style>
---
+++ src/view/Result.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="result">
+    <p>{{ length }}問中{{ correct }}問正解です。</p>
+    <p v-if="correct == 3">おめでとう。</p>
+    <p v-if="correct == 0">残念でした。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  props: {
+    correct: Number,
+    length: Number
+  }
+}
+</script>
+
+<style lang="scss">
+.result {
+  width: 100%;
+  padding: 160px 0;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  p {
+    font-size: 28px;
+    margin: 0 0 10px;
+  }
+}
+
+@media (max-width: 750px) {
+  .result {
+    p {
+      font-size: 24px;
+    }
+  }
+}
+</style>
